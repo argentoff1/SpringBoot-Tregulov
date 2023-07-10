654c261bbb4c6c12cd6a21bba2c4e9237d4055cf
argentoff1 <mparinos56@gmail.com>
2023-07-10 11:22:49 +0300

    Закомментирована Hibernate реализация, все работает через JPA

--- src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
@@ -1,9 +1,10 @@
 package com.zaurtregulov.spring.springboot.spring_course_springboot.dao;
 
 import com.zaurtregulov.spring.springboot.spring_course_springboot.entity.Employee;
+
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+import jakarta.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,9 +20,14 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     @Transactional
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
+        /*Session session = entityManager.unwrap(Session.class);
 
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        Query<Employee> query = session.createQuery("from Employee",
+                Employee.class);
+
+        List<Employee> allEmployees = query.getResultList();*/
+
+        Query query = entityManager.createQuery("from Employee");
         List<Employee> allEmployees = query.getResultList();
 
         return allEmployees;
@@ -29,25 +35,38 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
+        /*Session session = entityManager.unwrap(Session.class);
 
-        session.saveOrUpdate(employee);
+        session.saveOrUpdate(employee);*/
+
+        // При дефолтном создании записи, ID будет равен 0
+        Employee newEmployee = entityManager.merge(employee);
+
+        // ID будет инкрементироваться с помощью сеттера
+        employee.setId(newEmployee.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
+        /*Session session = entityManager.unwrap(Session.class);
 
-        Employee employee = session.get(Employee.class, id);
+        Employee employee = session.get(Employee.class, id);*/
+
+        Employee employee = entityManager.find(Employee.class, id);
 
         return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
+        /*Session session = entityManager.unwrap(Session.class);
 
         Query<Employee> query = session.createQuery("delete from Employee " +
+                "where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();*/
+
+        Query query = entityManager.createQuery("delete from Employee " +
                 "where id =:employeeId");
         query.setParameter("employeeId", id);
         query.executeUpdate();
